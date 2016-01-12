d10510c50783f5e40a0de3a220f95ddc6fcaa6fb
khmaralex <khmaraalex@gmail.com>
2016-01-12 23:56:42 +0300

    Using the Angular's ng-switch directive

--- app/controllers.js
+++ app/controllers.js
@@ -22,11 +22,6 @@ lodSite.controller('RandomProjectsController', ['$scope', '$http', function($sco
 lodSite.controller('FullProjectsController', ['$scope', '$http', function($scope, $http) {
   $http.get('http://api.lod-misis.ru/projects').success(function(data) {
     $scope.fullProjects = data;
-    for(var i = 0; i < $scope.fullProjects.length; i++){
-      $scope.fullProjects[i].ProjectStatus = ($scope.fullProjects[i].ProjectStatus === 0) ? 'запланирован' :
-        ($scope.fullProjects[i].ProjectStatus === 1) ? 'в процессе' :
-          ($scope.fullProjects[i].ProjectStatus === 2) ? 'заморожен' : 'завершён';
-    }
   });
 }]);
 
@@ -37,17 +32,7 @@ lodSite.controller('FooterController', ['$scope', function($scope) {
 lodSite.controller('ProjectController', ['$scope', '$http', function ($scope, $http) {
   $http.get('http://api.lod-misis.ru/projects/2').success(function (data) {
     $scope.project = data;
-    for (var i = 0; i < $scope.project.ProjectType.length; i++) {
-      $scope.project.ProjectType[i] = ($scope.project.ProjectType[i] === 0) ? 'Веб' :
-        ($scope.project.ProjectType[i] === 1) ? 'Мобильное' :
-          ($scope.project.ProjectType[i] === 2) ? 'Десктопное' :
-            ($scope.project.ProjectType[i] === 3) ? 'Игра' :
-              ($scope.project.ProjectType[i] === 4) ? 'Плагин' : 'Прочее';
-    }
-    console.log($scope.project.ProjectType);
-    $scope.project.ProjectStatus = ($scope.project.ProjectStatus === 0) ? 'запланирован' :
-      ($scope.project.ProjectStatus === 1) ? 'в процессе' :
-        ($scope.project.ProjectStatus === 2) ? 'заморожен' : 'завершён';
-    $scope.projectIssues = $scope.project.Issues.slice();
+    $scope.projectTypes = $scope.project.ProjectType;
+    $scope.projectIssues = $scope.project.Issues;
   })
 }]);
